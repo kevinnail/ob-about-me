e005137b29def1645937af7127cf5e77ddfccdf2
kevinnail <fumalicious@gmail.com>
2022-09-06 19:42:08 -0700

    Was playing around with some JS and forgot, cleared it out.

--- app.js
+++ app.js
@@ -6,14 +6,3 @@
 // get user input
 // use user input to update state
 // update DOM to reflect the new state
-
-// Event listeners
-
-openBtn.addEventListener('click', function () {
-    modalContainer.style.display = 'block';
-});
-
-hideBtn.addEventListener('click', function () {
-    modalContainer.style.display = 'none';
-    alert('hi');
-});

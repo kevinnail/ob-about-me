3995e099e837fab0dd44d281fabf8c7398fda8ac
kevinnail <fumalicious@gmail.com>
2022-09-06 18:13:22 -0700

    final touches done, exp with margins to help show background now, good place to revert!

--- app.js
+++ app.js
@@ -2,7 +2,18 @@
 
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+// Event listeners
+
+openBtn.addEventListener('click', function () {
+    modalContainer.style.display = 'block';
+});
+
+hideBtn.addEventListener('click', function () {
+    modalContainer.style.display = 'none';
+    alert('hi');
+});
